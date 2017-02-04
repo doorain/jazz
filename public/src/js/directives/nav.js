bbf5ed0784bad321cef7341fac7ad38c5ae58eb4
Dorian Morgan <doorain@gmail.com>
2017-02-04 01:36:36 -0700

    nav bar and hamburger menu setup

--- public/src/js/directives/nav.js
+++ public/src/js/directives/nav.js
@@ -1,11 +1,56 @@
 angular.module('jazz')
   .directive('navDirective', function() {
     var controller = function($scope, $state, mainService) {
-}
+
+// menu icon animation
+
+$(document).ready(function(){
+  $('#nav-icon3').click(function(){
+    $(this).toggleClass('open');
+  });
+}); 
+
+
+
+// menu dropdown animationm
+$scope.showLogin = function () {
+    $scope.modalFunc = !$scope.modalFunc;
+    console.log('firing')
+  }
+  $scope.menuShowing = false;
+  $scope.toggleMenu = function() {
+    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    if (!$scope.menuShowing) {
+        $('div.menu').addClass('animated slideInDown').one(animationEnd, function () {
+          $(this).removeClass('animated slideInDown')
+        });
+    }
+    else {
+          $('div.menu').addClass('animated slideOutUp').one(animationEnd, function () {
+            $(this).prev().find('#nav-icon3').removeClass('open')
+            $(this).removeClass('animated slideOutUp')
+
+        })
+    }
+    $scope.menuShowing = !$scope.menuShowing;
+    // console.log($scope.menuShowing);
+  }
+  $scope.closeMenu = function() {
+    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    if ($scope.menuShowing) {
+      $('div.menu').addClass('animated slideOutUp').one(animationEnd, function () {
+        $(this).prev().find('#nav-icon3').removeClass('open')
+        $(this).removeClass('animated slideOutUp')
+
+    });
+    }
+    $scope.menuShowing = false;
+  }
+};
     return {
       restrict: 'AE',
       controller: controller,
-      templateUrl: '../src/views/template/navTmpl.html'
+      templateUrl: '../src/views/templates/navTmpl.html'
     }
 
 
