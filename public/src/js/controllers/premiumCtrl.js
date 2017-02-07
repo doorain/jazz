eb9f55a64273ba332a2349167db4b95bc5e3a600
Dorian Morgan <doorain@gmail.com>
2017-02-07 00:50:52 -0700

    init

--- public/src/js/controllers/premiumCtrl.js
+++ public/src/js/controllers/premiumCtrl.js
@@ -1,6 +1,19 @@
 angular.module('jazz')
 .controller('premiumCtrl', function($scope, mainService){
 
-$scope.test = ' hello from ctrl';
+// $scope.test = ' hello from ctrl';
 
-})
+  $(document).ready(function(){
+    $('.fade').slick({
+      autoplay: true,
+      dots: false,
+      infinite: true,
+      speed: 1000,
+      fade: true,
+      cssEase: 'linear',
+      arrows: false
+    });
+
+  });
+
+});
